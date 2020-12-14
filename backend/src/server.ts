7493902d1596b00a725bfd0e9a995089f49c5051
eliasall3x <elliasallex@gmail.com>
2020-12-13 23:51:51 -0300

    feat: cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import routes from './routes'
@@ -5,7 +6,7 @@ dotenv.config({})
 
 const app = express()
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
-// import { Container } from './styles';
+interface IUser {
+  name: string;
+  email: string;
+}
 
 const App: React.FC = () => {
+  const [users, setUser] = useState<IUser[]>([])
+
+  useEffect(() => {
+    api.get<IUser[]>('users').then(response => {
+      setUser(response.data)
+    })
+  }, [])
+
   return (
     <div>
-      <h1>Elias alexandre</h1>
+      {users.map(user => (
+        <>
+          <strong>{user.name}</strong>
+          <p>{user.email}</p>
+        </>
+      ))}
     </div>
   );
 }
