c3b3b4ba13ff27ca88300ccda9c5763a6ae8d18e
eliasall3x <elliasallex@gmail.com>
2020-12-13 23:23:46 -0300

    DIST

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,8 @@ dotenv.config({})
 
 const app = express()
 
+// app.use(cors())
+app.use(express.json())
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => console.log(`RUNNING IN PORT: ${process.env.PORT || 3333}`))
